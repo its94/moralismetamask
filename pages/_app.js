45f5e4807c3fc0a587c1f405ee2c32289e9f284e
its94 <103844813+its94@users.noreply.github.com>
2022-06-28 21:14:59 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,9 @@ import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <MoralisProvider
-    appId="Enter your moralis server appID"
-    serverUrl="Enter your moralis server URL">
+   <MoralisProvider
+    appId="6CF08Q5FQ7apoq4tcVbyVR3Lh1vFRktHpE5cYTki"
+    serverUrl="https://9paos9ehpmff.usemoralis.com:2053/server">
       <Component {...pageProps} />
     </MoralisProvider>
   );
